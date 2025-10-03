2adee157d10d4849aa0e049a8210f4e572a5f1fe
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-10-03 15:42:48 +0530

    loginandSignUpcommit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/register' element = {<Register />}/>
+        <Route path='/login' element = {<Login />}/>
+      </Routes>
       
     </div>
   )
