d4b00327a83b1f8679266f9559e8fde37b31f65f
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-10-07 18:37:27 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,47 @@
-import express from "express";
-import dotenv from "dotenv";
-import http from "http"; // for creating the server
+import express from "express"
+import dotenv from "dotenv"
 import database from "./utils/database.js";
 import UserRouter from "./routes/UserRoute.js";
+
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
+import cors from "cors"
 import ImageRouter from "./routes/ImageRoute.js";
 import userProfileRoute from "./routes/ProfileRoute.js";
-import { Server } from "socket.io"; // <-- fixed import
 import LocationRoute from "./routes/LocationRoute.js";
 
-dotenv.config(); // load environment variables
 
 const app = express();
 
-// create server for socket.io
-const server = http.createServer(app);
-
-// initialize socket.io
-app.use(
-  cors({
-    origin: ["http://localhost:5174"], // ✅ correct frontend port
-    credentials: true,
-  })
-);
-
-
-// optional: socket connection events
-
-
 app.use(express.json());
-app.use(cookieParser());
+
+const port = process.env.PORT || 4000
+
+app.use(cookieParser())
 
 app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-  })
-);
+    cors({
+        origin:["http://localhost:5173"],
+        credentials: true
 
-database();
+    })
+)
 
-app.use("/api/v1", UserRouter);
-app.use("/api/v2", ImageRouter);
-app.use("/api/v3", userProfileRoute);
+database()
 
-app.use("/api/v4",LocationRoute)
+app.use("/api/v1",UserRouter);
 
-const port = process.env.PORT || 4000;
+app.use("/api/v2",ImageRouter);
 
-app.listen(port, () => {
-  console.log(`Server is Running at Port NO ${port}`);
-});
+app.use("/api/v3",userProfileRoute)
+
+app.use("api/v4",LocationRoute)
+
+app.listen(port,() => {
+    console.log(`Server is Running at Port NO ${port}`);
+})
+
+
+
 
 
